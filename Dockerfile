3719b71a0355eca1dc94fbd230de8f9f75673f8e
Hellyna NG <hellyna@groventure.com>
2015-10-07 16:16:41 +0800

    Many changes.
    + Removed template files to directly print the whole file instead.
    + Container now supports proxy mode, ie. forwarding of real ip via the PROXY_MODE variable

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,4 @@ FROM nginx:1.9
 MAINTAINER Hellyna NG <hellyna@groventure.com>
 
 COPY scripts/* /scripts/
-COPY conf/* /usr/local/etc/rattic/
-
 ENTRYPOINT ["/bin/bash", "/scripts/entrypoint.sh"]
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,19 +10,55 @@ if [[ -z "$UWSGI_PORT_8000_TCP_PORT" ]]; then
   exit 1
 fi
 
-nginx_conftmpl_path='/usr/local/etc/rattic/nginx.tmpl.conf'
 nginx_conf_path='/etc/nginx/conf.d/rattic.conf'
 
-install -Zm 0600 "$nginx_conftmpl_path" "$nginx_conf_path"
-sed -ir \
-  's/{{\s*uwsgi_host\s*}}/'"$UWSGI_PORT_8000_TCP_ADDR"'/g' \
-  "$nginx_conf_path"
+cat > "$nginx_conf_path" <<EOF
+upstream rattic {
+  server ${UWSGI_PORT_8000_TCP_ADDR}:${UWSGI_PORT_8000_TCP_PORT};
+}
 
-sed -ir \
-  's/{{\s*uwsgi_port\s*}}/'"$UWSGI_PORT_8000_TCP_PORT"'/g' \
-  "$nginx_conf_path"
+server {
+  listen 80 default_server;
+  server_name _;
+  client_max_body_size 5M;
+EOF
 
-rm -f /etc/nginx/conf.d/default.conf "${nginx_conf_path}r"
+if [[ "$PROXY_MODE" == 'true' || "$PROXY_MODE" == 1 ]]; then
+  cat >> "$nginx_conf_path" <<EOF
+  real_ip_header X-Forwarded-For;
+  real_ip_recursive on;
+  set_real_ip_from 172.17.0.0/16;
+EOF
+fi
+
+cat >> "$nginx_conf_path" <<EOF
+  location /static {
+    alias /srv/rattic/static;
+  }
+  location / {
+    uwsgi_pass  rattic;
+    uwsgi_param QUERY_STRING    \$query_string;
+    uwsgi_param REQUEST_METHOD  \$request_method;
+    uwsgi_param CONTENT_TYPE    \$content_type;
+    uwsgi_param CONTENT_LENGTH  \$content_length;
+
+    uwsgi_param REQUEST_URI     \$request_uri;
+    uwsgi_param PATH_INFO       \$document_uri;
+    uwsgi_param DOCUMENT_ROOT   \$document_root;
+    uwsgi_param SERVER_PROTOCOL \$server_protocol;
+    uwsgi_param HTTPS           \$https if_not_empty;
+
+    uwsgi_param REMOTE_ADDR     \$remote_addr;
+    uwsgi_param REMOTE_PORT     \$remote_port;
+    uwsgi_param SERVER_PORT     \$server_port;
+    uwsgi_param SERVER_NAME     \$server_name;
+  }
+  access_log /var/log/nginx/access.log;
+  error_log /var/log/nginx/error.log;
+}
+EOF
+
+rm -f /etc/nginx/conf.d/default.conf
 exec /usr/sbin/nginx -g 'daemon off;'
 
 set +e
